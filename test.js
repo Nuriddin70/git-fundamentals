8063b4a9346beabe7edf03623c1a2ca762ba8c25
Nuriddin70 <nuriddin3050@gmail.com>
2022-02-01 10:31:25 +0900

    Initial commit

---
+++ test.js
@@ -0,0 +1,67 @@
+"use strict";
+
+// const htmlPassed = false;
+// const cssPassed  = false;
+// let messege = '';
+
+// if (htmlPassed && cssPassed) {
+//     messege = 'Siz Bootstrap kursini boshlashingiz mumkin!';
+// } else if (htmlPassed || cssPassed) {
+//   messege = 'Iltimos ikkichi kursniham tugating!';
+// } else{
+//     messege='Ikkalasiniham tugat galcha!';
+// }
+
+// console.log(messege);
+
+
+//Function declaration
+// function enenOrOddDec(number) {
+    // if (number % 2 === 0) {
+    //     console.log('${number} id even');
+    // } else{
+    //     console.log('${number} id odd');
+    // }
+// }
+
+// enenOrOddDec(34)   
+
+
+//Function expression
+// const evenenOrOddExp = function(number) {
+//     if (number % 2 === 0) {
+//         console.log('${number} id even');
+//     } else{
+//         console.log('${number} id odd');
+//     } 
+// }
+
+// evenenOrOddExp(11)
+
+// const evenenOrOddArrow = number => {
+//     if (number % 2 === 0) {
+//         console.log('${number} id even');
+//     } else{
+//         console.log('${number} id odd');
+//     } 
+// }
+
+// evenenOrOddArrow(10)
+
+//Function declaration
+// function maxDec(number1, number2){
+//     if (number1 >= number2) {
+//     return number1;  
+//     }
+//     return number2;
+// } 
+
+// console.log( maxDec(5 ,2) ) 
+
+const answers = [];
+
+answers[0] = prompt("Ismingiz nima?");
+answers[1] = prompt("Ismingiz nima?");
+answers[2] = prompt("Ismingiz nima?");
+
+console.log(answers);
